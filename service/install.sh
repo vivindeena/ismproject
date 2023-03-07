f1f49a5246581007a6846a5ebd32b5f75ad8da1f
NikhilPatel42 <111683186+NikhilPatel42@users.noreply.github.com>
2023-03-07 22:38:44 +0530

    Update install.sh

--- service/install.sh
+++ service/install.sh
@@ -2,18 +2,22 @@
 #IN PROGRESS
 
 main() {
+    mkdir -p /var/log/test1/
+    touch /var/log/test1/time.log
+    touch /var/log/test1/audit.log
+
+    cp ./src/myWriter.service ./src/myWriter.timer /etc/systemd/system/
+    cp ./src/myWriter.sh /usr/bin/
+
     #specify required rules files here, specified a few sample files here.
     cp /usr/share/audit/sample-rules/44-installers.rules /etc/audit/rules.d/
     augenrules --load 
-
-    cp ./test/test1.service /etc/systemd/system
-
+    service auditd reload
+    service auditd start
+    
     printf "SUCCESS!\n"
     printf "Loaded Rules:\n"
     auditctl -l
-    systemctl start test1.service
-    systemctl enable test1.service
-
-    mkdir /var/log/test1/
-    touch /var/log/test1/time.log
+    systemctl start myWriter.service
+    systemctl enable myWriter.service
 }
