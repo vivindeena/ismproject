27448da532bbf057ff68c498f17dca29ef9fefe6
NikhilPatel42 <nikhil.patel.inter@gmail.com>
2023-03-19 22:31:26 +0530

    Update service/

--- service/install.sh
+++ service/install.sh
@@ -1,7 +1,28 @@
 #/bin/bash
 #IN PROGRESS
 
-main() {
+readandcurl() {
+
+    echo "Enter USERNAME:"
+    read USER
+    echo "Enter PASSWORD:"
+    read -s PSWD 
+    echo "JWT for $USER:$PSWD:"
+    curl --location --request POST 'localhost:3000/add' \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+        "client_username": "'$USER'",
+            "password" : "'$PSWD'"
+        }' > token.json
+
+    touch .env
+    jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' token.json > .env
+    rm token.json
+
+}
+
+installFiles() {
+
     mkdir -p /var/log/test1/
     touch /var/log/test1/time.log
     touch /var/log/test1/audit.log
@@ -10,14 +31,22 @@ main() {
     cp ./src/myWriter.sh /usr/bin/
 
     #specify required rules files here, specified a few sample files here.
-    cp /usr/share/audit/sample-rules/44-installers.rules /etc/audit/rules.d/
-    augenrules --load 
+    cp /usr/share/doc/auditd/examples/rules/40-local.rules /etc/audit/rules.d/
+    augenruleuses --load 
     service auditd reload
     service auditd start
-    
+
     printf "SUCCESS!\n"
     printf "Loaded Rules:\n"
     auditctl -l
     systemctl start myWriter.service
     systemctl enable myWriter.service
+
 }
+
+main() {
+    readandcurl
+    installFiles
+}
+
+main
--- service/src/myWriter.sh
+++ service/src/myWriter.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
-#writes a new audit log file after 
-#renaming the instantenous audit.log file - audit.log.$EPOCH;
 
-EPOCH=$(date "+%s")
-prename "s/audit.log/audit.log.${EPOCH}/" /var/log/test1/audit.log
-touch /var/log/test1/audit.log
-echo "$(date "+%s")" >> var/log/test1/time.log
+#runs on ubuntu
+#send an Email then
+#renaming the instantenous audit.log file - audit.log.$EPOCH; then
+#write a new audit log file 
 
-#email stuff under here? if needed?
+setEmail {
+
+    export $(xargs < /var/log/test1/time.log)
+    export $(xargs < ../.env)
+
+    SUBJECT="AUDIT LOG: $logtime"
+    BODY="$(date -d @$logtime)"
+
+    LOGFILE="/var/log/test1/audit.log"
+    #send this using -data-binary? use -F? to pass as attachment?
+
+    curl --location --request POST 'localhost:3000/send-mail' \
+        --header "Authorization: Bearer $token" \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+        "subject" : "'$SUBJECT'",
+            "body" : "'$BODY'"
+        }'
+
+}
+
+setNewLog {
+
+    EPOCH=$(date "+%s")
+    rename "s/audit.log/audit.log.${EPOCH}/" /var/log/test1/audit.log
+    touch /var/log/test1/audit.log
+    echo "logtime=$(date "+%s")" > var/log/test1/time.log
+
+}
+
+main {
+    setEmail
+    setNewLog
+}
+
+main
