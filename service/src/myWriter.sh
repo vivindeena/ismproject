5b103230a8c8cd3bb7dbd530a3a1af6bc3ebae26
Vivin D <vivin.deena@gmail.com>
2023-03-22 17:43:31 +0530

    Error Handling cURL
    Made some chagnes to the Writer to handle mail errros from the cURL
    requests

--- service/src/myWriter.sh
+++ service/src/myWriter.sh
@@ -13,21 +13,27 @@ setEmail() {
     BODY="$(date -d @$logtime)"
 
     LOGFILE="/var/log/test1/audit.log"
+    COUNT=0
+    while [ $COUNT -le 5 ]; do
+        CODE=$(curl -w '%{http_code}' --stderr my_err_file  -o output\
+            --header "Authorization: Bearer $token" \
+            --form "subject= $SUBJECT"\
+            --form "body= $BODY"\
+            --form "upload=@$LOGFILE"
+            )
+        cat output
+        rm output
 
-    CODE=$(curl -w '%{http_code}' --stderr my_err_file  -o output\
-        --header "Authorization: Bearer $token" \
-        --form "subject= $SUBJECT"\
-        --form "body= $BODY"\
-        --form "upload=@$LOGFILE"
-        )
-    cat output
-    rm output
-    if [[ "$CODE" =~ ^2 ]]; then
-        echo 
-        return 0;
-    else
-        echo 
-        return -1;
+        if [[ "$CODE" =~ ^2 ]]; then
+            break
+        else
+            ((COUNT++))
+            sleep 60
+        fi
+    done
+    if [[ $COUNT -eq 5]]; then
+        systemctl stop myService.service
+        echo "$(date -d @$logtime): Service Stopped because mails are not sent, check and restart the service with 'systemctl start myService.service'" >>/var/log/test1/log.err
     fi
 }
 
